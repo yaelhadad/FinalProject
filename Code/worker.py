d89aa92e09a2d15a7cc158b40d440c0c0e210a65
ANAEL <yael7100@gmail.com>
2021-09-06 02:46:35 +0300

    write the code with example

--- Code/worker.py
+++ Code/worker.py
@@ -2,29 +2,54 @@ REV_PER_HOUR = 0.5
 
 class Worker:
 
-
-    def __init__(self, name,password, rev_hours=0, qa_hours =0, new_hours=0, inprog_hours=0):
+    def __init__(self, name, expertise, impossible_tasks, availaiblity, availaiblity_start_sprint, count_current_work_hours,
+              current_tasks, password, optional_tasks, unique_tasks, rev_hours=0, dedicated_hours=0, qa_hours=0, total_prev_hours=0):
 
         self.name = name
         self.password = password
+        self.expertise = expertise
+        self.impossible_tasks = impossible_tasks
+        self.availaiblity_start_sprint =availaiblity_start_sprint
+        self.count_current_work_hours =count_current_work_hours
+        self.current_tasks= current_tasks
+        self.optional_tasks =optional_tasks
+        self.unique_tasks =unique_tasks
+        self.availaiblity = availaiblity
+        self.dedicated_hours =dedicated_hours
+        self.total_prev_hours =total_prev_hours
         self.rev_hours = rev_hours
         self.qa_hours = qa_hours
-        self.new_hours = new_hours
-        self.inprog_hours = inprog_hours
+
+
+    # def __init__(self, name,password, rev_hours=0, qa_hours =0, new_hours=0, inprog_hours=0):
+    #
+    #     self.name = name
+    #     self.password = password
+    #     self.rev_hours = rev_hours
+    #     self.qa_hours = qa_hours
+    #     self.new_hours = new_hours
+    #     self.inprog_hours = inprog_hours
 
 
     def update_review_time(self,num_tasks):
-
         self.rev_hours = num_tasks * REV_PER_HOUR
 
-
     def update_qa_time(self,qa_time):
-
         self.qa_hours = qa_time
 
-    def update_new_time(self,new_time):
+    def update_assigneed_uniqe_task(self,task):
+       self.unique_tasks.remove(task.id)
 
-        self.new_hours = new_time
+    def update_assigneed_task(self, task):
+        if task.priority == 'A':
+            self.availaiblity_start_sprint -= task.allotted_time
+        self.availaiblity -= task.allotted_time
+        self.count_current_work_hours +=task.allotted_time
+        self.current_tasks.append(task.id)
 
-    def update_inprog_time(self, inprog_time):
-            self.inprog_hours = inprog_time
+    def print_current(self):
+        print (self.name)
+        print (self.availaiblity)
+        print(self.availaiblity_start_sprint)
+        print (self.count_current_work_hours)
+        print(self.current_tasks)
