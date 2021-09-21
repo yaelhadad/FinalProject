0b79f78d53545834741c4856ee665f7c1c0ce58b
ANAEL <yael7100@gmail.com>
2021-09-22 01:21:45 +0300

    add max find algorithm

--- Code/Assign_Task.py
+++ Code/Assign_Task.py
@@ -1,10 +1,17 @@
+import numpy as np
 from Read_Config import all_workers
 
 TASK = "Task"
 IS_UNIQUE = "Is the task Unique?"
 ASSIGNED = 'assigned'
+STATUS = "Status"
 NAME = 'Name'
 BUDGET_UNIQUE = 'Total time for less important unique tasks'
+ONLY_ONE = 1
+ONE = -1
+AVAILABILITY = "availability"
+FIRST = 0
+
 
 
 def get_worker(name):
@@ -12,6 +19,22 @@ def get_worker(name):
         if name == worker.name:
             return worker
     return None
+
+
+def assign_task(task, worker):
+    task.set_task(ASSIGNED, worker.name)
+    worker.update_assigned_task(task)
+
+
+def find_worker_with_max_availability(df):
+    for idx, row in df.iterrows():
+        name = row.loc[NAME]
+        worker = get_worker(name)
+        df.at[idx, AVAILABILITY] = worker.availability
+    df = df.sort_values(by=[AVAILABILITY], ascending=False)
+    needless_indexes = df.index.values
+    return df.iloc[FIRST][NAME], np.delete(needless_indexes, FIRST)
+
 
 
 class Assign:
@@ -26,36 +49,59 @@ class Assign:
                 return task
         return None
 
-    def update_workers (self, index, min_budget_index):
-            set_difference = set(index) - set(min_budget_index)
-            list_difference = list(set_difference)
-            self.config_file = self.config_file.drop(list_difference)
-
-
-    def assign_task(self, task, worker):
-
-        task.set_task(ASSIGNED, worker.name)
-        worker.update_assigned_task(task)
+    def update_workers(self, index, min_budget_index):
+        set_difference = set(index) - set(min_budget_index)
+        list_difference = list(set_difference)
+        self.config_file = self.config_file.drop(list_difference)
 
     def decide_who_will_assign(self, task):
-        df = self.config_file.loc[self.config_file[TASK] == task.name]
-        df_min_budget = df.sort_values(by=[BUDGET_UNIQUE]).head(1)
-        for idx, row in df_min_budget.iterrows():
+        df_possible_workers = self.config_file.loc[self.config_file[TASK] == task.name]
+        for idx, row in df_possible_workers.iterrows():
             name = row.loc[NAME]
-        self.assign_task(task, get_worker(name))
-        self.update_workers(df.index, df_min_budget.index)
+            budget = row.loc[BUDGET_UNIQUE]
+            worker = get_worker(name)
+            if not worker.verify_optional_task_before_devide(task, budget):
+                df_possible_workers = df_possible_workers.drop(idx)
+                self.config_file = self.config_file.drop(idx)
+        if len(df_possible_workers.index) == ONLY_ONE and task.status != ASSIGNED:
+            assign_task(task, get_worker(df_possible_workers.iloc[ONE][NAME]))
+            return
+        df_min_budget = df_possible_workers.sort_values(by=[BUDGET_UNIQUE])
+        # Case that there is only one worker with the most minimal budget
+        if df_min_budget.iloc[0][BUDGET_UNIQUE] != df_min_budget.iloc[1][BUDGET_UNIQUE]:
+            assign_task(task, get_worker(df_min_budget.head(1).iloc[ONE][NAME]))
+            self.update_workers(df_possible_workers.index, df_min_budget.head(1).index)
+            return
+        else:
+            # Case that there are many workers with the most minimal budget
+            # Pick by the max availability
+            worker, index_drop = find_worker_with_max_availability(df_min_budget)
+            assign_task(task, get_worker(worker))
+            self.config_file = self.config_file.drop(index_drop)
+            print(worker)
+            print(task.name)
+
+        # df_min_budget = df_possible_workers.sort_values(by=[BUDGET_UNIQUE]).head(1)
+        # for idx, row in df_min_budget.iterrows():
+        #     name = row.loc[NAME]
+        # assign_task(task, get_worker(name))
+        # self.update_workers(df_possible_workers.index, df_min_budget.index)
 
     def run(self):
-        self.config_file = self.config_file.sort_values(by=[TASK])
         print(self.config_file)
-        print(self.all_tasks)
-        print(all_workers)
+        self.config_file.to_csv(
+            r"C:\Users\Yael Hadad\PycharmProjects\FinalProject\code_for_project\Tests\Test5\before_assign_gold.csv")
+        self.config_file = self.config_file.sort_values(by=[TASK])
         # Check if the task is unique:
         for idx, row in self.config_file.iterrows():
+            task = self.get_task(row.loc[TASK])
             if row.loc[IS_UNIQUE]:
-                self.assign_task(self.get_task(row.loc[TASK]), get_worker(row.loc[NAME]))
-            if row.loc[IS_UNIQUE] == False:
+                assign_task(task, get_worker(row.loc[NAME]))
+            # Decide what to do if a task appear in the optional list of multiple workers
+            if not row.loc[IS_UNIQUE] and task.status != ASSIGNED:
                 self.decide_who_will_assign(self.get_task(row.loc[TASK]))
         for worker in all_workers:
             worker.print_current()
         print(self.config_file)
+        self.config_file.to_csv(
+            r"C:\Users\Yael Hadad\PycharmProjects\FinalProject\code_for_project\Tests\Test5\after_assign_gold.csv")
