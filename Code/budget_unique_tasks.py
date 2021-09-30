1848eca2f296fb7f35f670b9008623adbd5cdec7
ANAEL <yael7100@gmail.com>
2021-10-01 00:39:04 +0300

    renmame Budget_Unique_Tasks.py to budget_uniuqe_tasks.py

---
+++ Code/budget_unique_tasks.py
@@ -0,0 +1,22 @@
+from constants import Constants
+
+
+class BudgetUnique:
+
+    def __init__(self, config_file):
+        self.config_file = config_file
+
+    def run(self):
+        self.config_file = self.config_file.sort_values(by=[Constants.NAME, Constants.TASK], ascending=False)
+        all_workers = (self.config_file[Constants.NAME].unique())
+        for worker in all_workers:
+            df = self.config_file.loc[self.config_file[Constants.NAME] == worker]
+            count_budget = 0
+            for idx, row in df.iterrows():
+                if row.loc[Constants.IS_UNIQUE]:
+                    count_budget = count_budget + float(row.loc[Constants.ALLOTTED_TIME])
+                else:
+                    self.config_file.at[idx, Constants.BUDGET_UNIQUE] = str(count_budget)
+        self.config_file.to_csv(
+            r"C:\Users\Yael Hadad\PycharmProjects\FinalProject\Try_Pandas\sprint_23_9\BUDGET_BUGS.csv")
+        return self.config_file
