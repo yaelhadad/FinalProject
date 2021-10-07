8fa45229a33e6524d42211022a7778ef3f6a3fa9
ANAEL <yael7100@gmail.com>
2021-10-08 01:18:54 +0300

    add another tests

--- Code/test.py
+++ Code/test.py
@@ -3,20 +3,34 @@ import pandas as pd
 import filecmp
 import io
 
-from validate import Valid
+from validate import ValidWorkersFile, ValidTasksFile
 from generate_tasks import GenerateTask
 from constants import Constants
 from processing_workers_info import WorkerInfo
 from pandas.testing import assert_frame_equal
 
 
-class TestValid(unittest.TestCase):
+class TestValidWorkersFile(unittest.TestCase):
 
-    def test1(self):
-        v = Valid(
+    def test_invalid_worker_name(self):
+        workers_file = ValidWorkersFile(
             pd.read_csv(f"{Constants.root_dir}/{Constants.valid_workers_info}/{Constants.workers_info_not_found}"))
         with self.assertRaises(ValueError):
-            v.valid_values()
+            workers_file.valid_values()
+
+    def test_empty_table(self):
+        workers_file = ValidWorkersFile(
+            pd.read_csv(f"{Constants.root_dir}/{Constants.valid_workers_info}/{Constants.workers_empty}"))
+        with self.assertRaises(ValueError):
+            workers_file.valid_values()
+
+
+class TestValidTasksFile(unittest.TestCase):
+    def test_empty_table(self):
+        tasks = ValidTasksFile(
+            pd.read_csv(f"{Constants.root_dir}/{Constants.valid_tasks}/{Constants.tasks_empty}"))
+        with self.assertRaises(ValueError):
+            tasks.valid_values()
 
 
 class TestGenerateTask(unittest.TestCase):
@@ -62,16 +76,43 @@ class TestWorkerInfo(unittest.TestCase):
     def test_heartbeat(self):
         tasks = GenerateTask(
             pd.read_csv(f"{Constants.root_dir}/{Constants.generate_tasks}/{Constants.tasks_9_same_priority}"))
-        pos_tasks = WorkerInfo(pd.read_csv(f"{Constants.root_dir}/{Constants.worker_info}/{Constants.work_table}"),tasks.all_tasks)
+        pos_tasks = WorkerInfo(pd.read_csv(f"{Constants.root_dir}/{Constants.worker_info}/{Constants.work_table}"),
+                               tasks.all_tasks)
         df = pd.read_csv(f"{Constants.root_dir}/{Constants.worker_info_gold}/{Constants.proc_tasks9}")
         assert_frame_equal(df, pos_tasks.df_tasks_db)
 
-    def test_1_assign(self):
+    def test_1_task_some_workers_can_do_it(self):
         tasks = GenerateTask(
             pd.read_csv(f"{Constants.root_dir}/{Constants.generate_tasks}/{Constants.tasks_1}"))
-        pos_tasks = WorkerInfo(pd.read_csv(f"{Constants.root_dir}/{Constants.worker_info}/{Constants.work_table}"),tasks.all_tasks)
+        pos_tasks = WorkerInfo(pd.read_csv(f"{Constants.root_dir}/{Constants.worker_info}/{Constants.work_table}"),
+                               tasks.all_tasks)
         df = pd.read_csv(f"{Constants.root_dir}/{Constants.worker_info_gold}/{Constants.proc_tasks1}")
         assert_frame_equal(df, pos_tasks.df_tasks_db)
+
+    def test_1_task_all_workers_can_do_it(self):
+        tasks = GenerateTask(
+            pd.read_csv(f"{Constants.root_dir}/{Constants.generate_tasks}/{Constants.tasks_1}"))
+        pos_tasks = WorkerInfo(pd.read_csv(f"{Constants.root_dir}/{Constants.worker_info}/{Constants.work_table_all}"),
+                               tasks.all_tasks)
+        df = pd.read_csv(f"{Constants.root_dir}/{Constants.worker_info_gold}/{Constants.proc_tasks1_all}")
+        assert_frame_equal(df, pos_tasks.df_tasks_db)
+
+    def test_1_task_workers_can_not_do_it_time(self):
+        tasks = GenerateTask(
+            pd.read_csv(f"{Constants.root_dir}/{Constants.generate_tasks}/{Constants.task_1_impossible_hours}"))
+        with self.assertRaises(ValueError):
+            pos_tasks = WorkerInfo(
+                pd.read_csv(f"{Constants.root_dir}/{Constants.worker_info}/{Constants.work_table_all}"),
+                tasks.all_tasks)
+
+    def test_1_task_workers_can_not_do_it_expertise(self):
+        tasks = GenerateTask(
+            pd.read_csv(f"{Constants.root_dir}/{Constants.generate_tasks}/{Constants.task_1_impossible_expertise}"))
+        with self.assertRaises(ValueError):
+            pos_tasks = WorkerInfo(
+                pd.read_csv(f"{Constants.root_dir}/{Constants.worker_info}/{Constants.work_table_all}"),
+                tasks.all_tasks)
+
 
 if __name__ == '__main__':
     unittest.main()
