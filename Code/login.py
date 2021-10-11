cee138e4fc698f368a94dded305a822c936b292e
ANAEL <yael7100@gmail.com>
2021-10-11 10:49:49 +0300

    add assign button

--- Code/login.py
+++ Code/login.py
@@ -1,23 +1,25 @@
 from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_bootstrap import Bootstrap
-from wtforms import StringField,PasswordField, BooleanField
+from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
 from flask_wtf import FlaskForm
-from flask_sqlalchemy  import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 import os
 import sqlite3
 from flask_socketio import SocketIO
 import pandas as pd
-#import main
+import sys, traceback
+import subprocess
+# import main
 import email_validator
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
 app.config['UPLOAD_FOLDER'] = r"C:\Users\Yael Hadad\PycharmProjects\FinalProject\code_for_project\static\Excel"
-app.secret_key="123"
+app.secret_key = "123"
 
 db = SQLAlchemy(app)
 login_manager = LoginManager()
@@ -33,23 +35,28 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(80))
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
     remember = BooleanField('remember me')
 
+
 class RegisterForm(FlaskForm):
     email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -60,6 +67,7 @@ def login():
             login_user(user, remember=form.remember.data)
             return redirect(url_for('ExcelUpload'))
     return render_template('login.html', form=form)
+
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -74,11 +82,12 @@ def signup():
     return render_template('signup.html', form=form)
 
 
-con=sqlite3.connect('MyData3.db')
+con = sqlite3.connect('MyData3.db')
 con.execute("create table if not exists data(pid integer primary key,exceldata TEXT)")
 con.close()
 
-@app.route('/ExcelUpload',methods=['GET','POST'])
+
+@app.route('/ExcelUpload', methods=['GET', 'POST'])
 @login_required
 def ExcelUpload():
     if request.method == 'POST':
@@ -102,21 +111,22 @@ def ExcelUpload():
     return render_template("ExcelUpload.html")
 
 
-
 @app.route('/view_excel/<string:id>')
 def view_excel(id):
     con = sqlite3.connect("MyData3.db")
     con.row_factory = sqlite3.Row
     cur = con.cursor()
-    cur.execute("select * from data where pid=?",(id))
+    cur.execute("select * from data where pid=?", (id))
     data = cur.fetchall()
     print(data)
     for val in data:
-        path = os.path.join("static/Excel/",val[1])
+        path = os.path.join("static/Excel/", val[1])
         print(val[1])
-        data=pd.read_csv(path)
+        data = pd.read_csv(path)
     con.close()
-    return render_template("view_excel.html",data=data.to_html(index=False,classes="table table-bordered").replace('<th>','<th style="text-align:center">'))
+    return render_template("view_excel.html",
+                           data=data.to_html(index=False, classes="table table-bordered").replace('<th>',
+                                                                                                  '<th style="text-align:center">'))
 
 
 @app.route('/logout')
@@ -124,19 +134,45 @@ def view_excel(id):
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
 @app.route('/delete_record/<string:id>')
 def delete_record(id):
     try:
-        con=sqlite3.connect("MyData3.db")
-        cur=con.cursor()
-        cur.execute("delete from data where pid=?",[id])
+        con = sqlite3.connect("MyData3.db")
+        cur = con.cursor()
+        cur.execute("delete from data where pid=?", [id])
         con.commit()
-        flash("Record Deleted Successfully","success")
-    except:
-        flash("Record Deleted Failed", "danger")
-    finally:
+        flash("Record Deleted Successfully", "success")
         return redirect(url_for("ExcelUpload"))
         con.close()
+    except:
+        flash("Record Deleted Failed", "danger")
+    # finally:
+    #     return redirect(url_for("ExcelUpload"))
+    #     con.close()
+
+
+@app.route('/assign', methods=['GET', 'POST'])
+def assign():
+    arr = []
+    for root, dirs, files in os.walk(os.path.abspath("./static/Excel")):
+        for file in files:
+            arr.append(os.path.join(root, file))
+    print(arr[0], arr[1])
+    tasks = arr[1].replace(r"C:\Users\Yael Hadad\PycharmProjects\FinalProject\code_for_project", '.')
+    workers = arr[0].replace(r"C:\Users\Yael Hadad\PycharmProjects\FinalProject\code_for_project", '.')
+    print (tasks, workers)
+    # subprocess.call("python main.py -tasks %s -workers %s" % (arr[1], arr[0]), shell=True)
+    try:
+        os.system("python main.py -tasks %s -workers %s" % (tasks, workers))
+    except Exception:
+        print("Exception in user code:")
+        print("-" * 60)
+        traceback.print_exc(file=sys.stdout)
+        print("-" * 60)
+    return redirect(url_for('index'))
+
+
 if __name__ == '__main__':
     app.run(debug=False)
-
