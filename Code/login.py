bf85b56ca374e69b46c51fecf4b77f2ecc293984
unknown <yael7100@gmail.com>
2021-10-15 02:34:08 +0300

    one db

--- Code/login.py
+++ Code/login.py
@@ -9,7 +9,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import os
 import sqlite3
 import pandas as pd
+from io import TextIOWrapper
 import sys, traceback
+import csv
 import email_validator
 
 app = Flask(__name__)
@@ -24,10 +26,10 @@ login_manager.init_app(app)
 login_manager.login_view = 'login'
 Bootstrap(app)
 
-
-con = sqlite3.connect('Input_Files.db')
-con.execute("create table if not exists data(pid integer primary key,exceldata TEXT)")
-con.close()
+#
+# con = sqlite3.connect('Input_Files.db')
+# con.execute("create table if not exists data(pid integer primary key,exceldata TEXT)")
+# con.close()
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -35,9 +37,25 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(80))
 
-class Temp(UserMixin, db.Model):
+class Tasks(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    banana = db.Column(db.String(15), unique=True)
+    ID_Task = db.Column(db.String(4))
+    Status = db.Column(db.String)
+    Description = db.Column(db.String)
+    Subject = db.Column(db.String)
+    Assignee = db.Column(db.String)
+    Queue = db.Column(db.String)
+    Allotted_time = db.Column(db.String)
+    Review_Time = db.Column(db.String)
+
+class Workers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    Name = db.Column(db.String)
+    Role = db.Column(db.String)
+    Total_hours = db.Column(db.String)
+    Total_hours_at_begin = db.Column(db.String)
+    Expertise = db.Column(db.String)
+
 
 
 class LoginForm(FlaskForm):
@@ -49,7 +67,7 @@ class LoginForm(FlaskForm):
 class RegisterForm(FlaskForm):
     email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=10)])
 
 # def connect_input_file():
 #     con = sqlite3.connect("Input_Files.db")
@@ -74,9 +92,9 @@ def signup():
     if form.validate_on_submit():
         hashed_password = generate_password_hash(form.password.data, method='sha256')
         new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-        new_banana = Temp(banana="new")
+        #new_banana = Temp(banana="new")
         db.session.add(new_user)
-        db.session.add(new_banana)
+        # db.session.add(new_banana)
         db.session.commit()
         return '<h1> New user'
 
@@ -106,68 +124,82 @@ def logout():
 @login_required
 def ExcelUpload():
     if request.method == 'POST':
-        uploadExcel = request.files['uploadExcel']
-        if uploadExcel.filename != '':
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], uploadExcel.filename)
-            uploadExcel.save(filepath)
-            con = sqlite3.connect("Input_Files.db")
-            cur = con.cursor()
-            cur.execute("insert into data(exceldata)values(?)", (uploadExcel.filename,))
-            con.commit()
-            flash("Excel Sheet Upload Successfully", "success")
-            con = sqlite3.connect("Input_Files.db")
-            con.row_factory = sqlite3.Row
-            cur = con.cursor()
-            cur.execute("select * from data")
-            data = cur.fetchall()
-            con.close()
-            return render_template("ExcelUpload.html", data=data)
-    else:
-        con = sqlite3.connect("Input_Files.db")
-        con.row_factory = sqlite3.Row
-        cur = con.cursor()
-        cur.execute("select * from data")
-        data = cur.fetchall()
-        con.close()
-        return render_template("ExcelUpload.html", data=data)
-    return redirect(url_for('ExcelUpload'))
+        tasks_csv = request.files['upload_tasks']
+        workers_csv = request.files['upload_workers']
+        tasks_csv = TextIOWrapper(tasks_csv, encoding='utf-8')
+        workers_csv = TextIOWrapper(workers_csv, encoding='utf-8')
+        csv_reader_tasks = csv.reader(tasks_csv, delimiter=',')
+        csv_reader_workers = csv.reader(workers_csv, delimiter=',')
+        #if request.form.get('upload_tasks'):
+        if db.session.query(Tasks).count() < 1:
+                for row in csv_reader_tasks:
+                    task = Tasks(ID_Task=row[0],Status =row[1], Description =row[2], Subject=row[3],Assignee=row[4],Queue=row[5],
+                             Allotted_time = row[6], Review_Time=row[7])
+                    db.session.add(task)
+                    db.session.commit()
+  #              return redirect(url_for('ExcelUpload'))
+
+        else:
+                db.session.query(Tasks).delete()
+                db.session.commit()
+                for row in csv_reader_tasks:
+                    task = Tasks(ID_Task=row[0],Status =row[1], Description =row[2], Subject=row[3],Assignee=row[4],Queue=row[5],
+                                 Allotted_time = row[6], Review_Time=row[7])
+                    db.session.add(task)
+                    db.session.commit()
+           #     return redirect(url_for('ExcelUpload'))
+        #if request.form.get('upload_workers'):
+        if db.session.query(Workers).count() < 1:
+                for row in csv_reader_workers:
+                    worker = Workers(Name=row[0],Role =row[1], Total_hours =row[2], Total_hours_at_begin=row[3], Expertise =row[4])
+                    db.session.add(worker)
+                    db.session.commit()
+             #   return redirect(url_for('ExcelUpload'))
+
+        else:
+                db.session.query(Workers).delete()
+                db.session.commit()
+                for row in csv_reader_workers:
+                    worker = Workers(Name=row[0],Role =row[1], Total_hours =row[2], Total_hours_at_begin=row[3], Expertise =row[4])
+                    db.session.add(worker)
+                    db.session.commit()
+        return redirect(url_for('ExcelUpload'))
+
+    return render_template("ExcelUpload.html")
 
 
 
-@app.route('/view_excel/<string:id>')
-def view_excel(id):
-    con = sqlite3.connect("Input_Files.db")
-    con.row_factory = sqlite3.Row
-    cur = con.cursor()
-    cur.execute("select * from data where pid=?", (id))
-    data = cur.fetchall()
-    print(data)
-    for val in data:
-        path = os.path.join("static/Excel/", val[1])
-        print(val[1])
-        data = pd.read_csv(path)
-    con.close()
-    return render_template("view_excel.html",
-                           data=data.to_html(index=False, classes="table table-bordered").replace('<th>',
-                                                                                                  '<th style="text-align:center">'))
-
-
-
-@app.route('/delete_record/<string:id>')
-def delete_record(id):
-    try:
-        con = sqlite3.connect("Input_Files.db")
-        cur = con.cursor()
-        cur.execute("delete from data where pid=?", [id])
-        con.commit()
-        flash("Record Deleted Successfully", "success")
-        return redirect(url_for("ExcelUpload"))
-        con.close()
-    except:
-        flash("Record Deleted Failed", "danger")
-    # finally:
-    #     return redirect(url_for("ExcelUpload"))
-    #     con.close()
+# @app.route('/view_excel/<string:id>')
+# def view_excel(id):
+#     con = sqlite3.connect("Input_Files.db")
+#     con.row_factory = sqlite3.Row
+#     cur = con.cursor()
+#     cur.execute("select * from data where pid=?", (id))
+#     data = cur.fetchall()
+#     print(data)
+#     for val in data:
+#         path = os.path.join("static/Excel/", val[1])
+#         print(val[1])
+#         data = pd.read_csv(path)
+#     con.close()
+#     return render_template("view_excel.html",
+#                            data=data.to_html(index=False, classes="table table-bordered").replace('<th>',
+#                                                                                                   '<th style="text-align:center">'))
+#
+#
+#
+# @app.route('/delete_record/<string:id>')
+# def delete_record(id):
+#     try:
+#         con = sqlite3.connect("Input_Files.db")
+#         cur = con.cursor()
+#         cur.execute("delete from data where pid=?", [id])
+#         con.commit()
+#         flash("Record Deleted Successfully", "success")
+#         return redirect(url_for("ExcelUpload"))
+#         con.close()
+#     except:
+#         flash("Record Deleted Failed", "danger")
 
 
 @app.route('/assign', methods=['GET', 'POST'])
@@ -195,4 +227,5 @@ def exit_view():
     return redirect(url_for("ExcelUpload"))
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=False)
