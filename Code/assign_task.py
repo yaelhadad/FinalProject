4e78841d1a47868375566c6cef0b3b1b2c9dcfb9
ANAEL <yael7100@gmail.com>
2021-10-19 13:28:37 +0300

    continuew working on the last task assigned page

--- Code/assign_task.py
+++ Code/assign_task.py
@@ -1,4 +1,5 @@
 import numpy as np
+from processing_workers_info import update_impossible_tasks, create_db_impossible_tasks
 from constants import Constants
 
 
@@ -39,20 +40,30 @@ class Assign:
             name = row.loc[Constants.NAME]
             budget = row.loc[Constants.BUDGET_UNIQUE]
             worker = self.all_workers[name]
-            print (worker.availability_start_sprint)
-            print(worker.availability)
+            # print (worker.availability_start_sprint)
+            # print(worker.availability)
             if not worker.verify_optional_task_before_devide(task, budget):
                 df_possible_workers = df_possible_workers.drop(idx)
                 self.remove_needless_optional_workers(idx)
+        if self.all_impossible_tasks.empty:
+              print(self.all_impossible_tasks)
+        else:
+            print(self.all_impossible_tasks)
         if df_possible_workers.empty:
-            self.all_impossible_tasks[task.name] = task
+            if self.all_impossible_tasks.empty:
+                self.all_impossible_tasks=create_db_impossible_tasks()
+            update_impossible_tasks(self.all_impossible_tasks,int(task.identifier),task.subject, task.description,
+                                    float(task.allotted_time))
+            print(self.config_file)
+            self.config_file = self.config_file[self.config_file.ID != task.identifier]
+            task.status = Constants.IMPOSSIBLE
             return
         if len(df_possible_workers.index) == Constants.ONLY_ONE and task.status != Constants.ASSIGNED:
             assign_task(task, self.all_workers[df_possible_workers.iloc[Constants.ONE_AND_LAST][Constants.NAME]])
             return
         df_min_budget = df_possible_workers.sort_values(by=[Constants.BUDGET_UNIQUE])
         # Case that there is only one worker with the most minimal budget
-        print(task.name)
+        # print(task.name)
         if df_min_budget.iloc[Constants.FIRST][Constants.BUDGET_UNIQUE] != df_min_budget.iloc[Constants.SECOND][Constants.BUDGET_UNIQUE]:
             assign_task(task, self.all_workers[df_min_budget.head(Constants.ONLY_ONE).iloc[Constants.ONE_AND_LAST][Constants.NAME]])
             self.update_workers(df_possible_workers.index, df_min_budget.head(Constants.ONLY_ONE).index)
@@ -76,7 +87,7 @@ class Assign:
             if row.loc[Constants.IS_UNIQUE]:
                 assign_task(task, self.all_workers[row.loc[Constants.NAME]])
             # Decide what to do if a task appear in the optional list of multiple workers
-            if not row.loc[Constants.IS_UNIQUE] and task.status != Constants.ASSIGNED:
+            print(task.identifier)
+            if not row.loc[Constants.IS_UNIQUE] and task.status not in (Constants.ASSIGNED, Constants.IMPOSSIBLE):
                 self.decide_who_will_assign(self.all_tasks[row.loc[Constants.TASK]])
-        # self.config_file.to_csv(
-        #     r"C:\Users\Yael Hadad\PycharmProjects\FinalProject\code_for_project\Tests\Test6\after_assign_gold.csv")
+
